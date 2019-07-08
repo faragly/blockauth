f30332755cc876cce46eb52342f6017730b5ea1f
Khalik Faradzhli <khalik.faradzhli@sysqual.net>
2019-07-08 13:43:26 +0500

    Сборка сайта

---
+++ docs/precache-manifest.b99a8349dac8ce8d12d45aafbac3fbd2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce50d7beaa0404470e3c4dc3b25eec87",
+    "url": "/blockauth/index.html"
+  },
+  {
+    "revision": "ef60ce1812d55eea4cba",
+    "url": "/blockauth/static/js/2.0a3512c6.chunk.js"
+  },
+  {
+    "revision": "85cc0f9954aad254c560",
+    "url": "/blockauth/static/js/main.d381c41e.chunk.js"
+  },
+  {
+    "revision": "baca2aef767a6b041414",
+    "url": "/blockauth/static/js/runtime~main.8ee160fb.js"
+  },
+  {
+    "revision": "91624b22b70c4feb90facd0fa916f386",
+    "url": "/blockauth/static/media/fingerprint-with-key.91624b22.svg"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bdcab2c45e3455fef35f1ed5f1c402e2.js"
+  "/blockauth/precache-manifest.b99a8349dac8ce8d12d45aafbac3fbd2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/blockauth/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
